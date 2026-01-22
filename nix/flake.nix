ce56917c9fe46bb4c39e91761f439808e56ccb14
araooz <araozfali@gmail.com>
2026-01-22 17:45:24 -0500

    puse urtl era url

--- nix/flake.nix
+++ nix/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    zen-browser.utrl = "github:youwen5/zen-browser-flake";
+    zen-browser.url = "github:youwen5/zen-browser-flake";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
