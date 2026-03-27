47e9c8c00651ead19d2c769f9ae22be048404aae
araooz <araozfali@gmail.com>
2026-03-26 21:43:17 -0500

    nvim cambio de colorscheme a un tal 'vague'

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,28 +1,44 @@
-local function enable_transparency()
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NvimTreeNormalNC", { bg = "none" })
-end
-
 return {
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme("tokyonight")
-			enable_transparency()
-		end,
-	},
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		opts = {
-			theme = "tokyonight",
-		},
-	},
+    {
+        "vague-theme/vague.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("vague").setup({
+                transparent = true,
+                bold = true,
+                italic = true,
+
+                on_highlights = function(highlights, colors) end,
+
+                colors = {
+                    bg = "#141415",
+                    inactiveBg = "#1c1c24",
+                    fg = "#cdcdcd",
+                    floatBorder = "#878787",
+                    line = "#252530",
+                    comment = "#606079",
+                    builtin = "#b4d4cf",
+                    func = "#c48282",
+                    string = "#e8b589",
+                    number = "#e0a363",
+                    property = "#c3c3d5",
+                    constant = "#aeaed1",
+                    parameter = "#bb9dbd",
+                    visual = "#333738",
+                    error = "#d8647e",
+                    warning = "#f3be7c",
+                    hint = "#7e98e8",
+                    operator = "#90a0b5",
+                    keyword = "#6e94b2",
+                    type = "#9bb4bc",
+                    search = "#405065",
+                    plus = "#7fa563",
+                    delta = "#f3be7c",
+                },
+            })
+            vim.cmd("colorscheme vague")
+        end
+    },
+
 }
