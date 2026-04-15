446f8de8d11beb3e48c65be006397f3bba89462b
araooz <araozfali@gmail.com>
2026-04-15 11:39:34 -0500

    Ctrl + Click abre un link desde neovim a zen

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -12,3 +12,28 @@ vim.keymap.set("v", "<C-c>", '"+y', { desc = "Copiar al portapapeles del sistema
 
 -- Limpiar notificaciones de Noice y Notify
 vim.keymap.set("n", "<leader>nd", "<cmd>Noice dismiss<cr>", { desc = "Descartar notificaciones" })
+
+
+-- Abrir links con Ctrl + Click usando Zen Browser
+vim.keymap.set("n", "<C-LeftMouse>", function()
+    local col = vim.api.nvim_win_get_cursor(0)[2] + 1
+    local line = vim.api.nvim_get_current_line()
+
+    -- Buscamos la URL que esté bajo el cursor o cerca de él
+    local url = line:match("https?://[%w%-_%.%?%+=&/%%#]+", 1)
+
+    -- Si encontramos una URL, verificamos que el cursor esté sobre ella (opcional pero preciso)
+    if url then
+        vim.fn.jobstart({ "zen", url }, {
+            detach = true,
+            on_exit = function(_, exit_code)
+                if exit_code ~= 0 then
+                    vim.notify("Error: No se pudo ejecutar el comando 'zen'", vim.log.levels.ERROR)
+                end
+            end,
+        })
+    end
+end, { desc = "Abrir link con Zen Browser (Ctrl+Click)" })
+
+-- También configuramos 'gx' para que use Zen por defecto
+vim.g.netrw_browsex_viewer = "zen"
