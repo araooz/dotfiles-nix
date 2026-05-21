7f992a8d16b715196f5a78f9f12af66bef560a02
araooz <araozfali@gmail.com>
2026-05-21 08:39:00 -0500

    solucionado en oneliners.lua el warning de mrd que decia 'undefined global vim'

--- nvim/lua/plugins/oneliners.lua
+++ nvim/lua/plugins/oneliners.lua
@@ -1,13 +1,27 @@
 return {
-    {	--This helps with ssh tunneling and copying to clipboard
-	'ojroques/vim-oscyank',
-    },{ --Git plugin
-	'tpope/vim-fugitive',
-    },{ --Show CSS Colors
-	'brenoprata10/nvim-highlight-colors',
-	config = function()
-	    require('nvim-highlight-colors').setup({})
-	end
-    },
+    {    --This helps with ssh tunneling and copying to clipboard
+        'ojroques/vim-oscyank',
+    }, { --Git plugin
+    'tpope/vim-fugitive',
+}, {     --Show CSS Colors
+    'brenoprata10/nvim-highlight-colors',
+    config = function()
+        require('nvim-highlight-colors').setup({})
+    end
+},
+
+
+    -- Source - https://stackoverflow.com/a/79656109
+    -- Posted by Jo Totland
+    -- Retrieved 2026-05-21, License - CC BY-SA 4.0
+    vim.lsp.config("lua_ls", {
+        settings = {
+            Lua = {
+                diagnostics = {
+                    globals = { "vim" } }
+            }
+        }
+    })
+
 
 }
