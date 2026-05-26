dcf4fbde5bf18d1af5f3843ec82a945802a47a8f
araooz <araozfali@gmail.com>
2026-05-26 14:29:13 -0500

    ensure installed ahora en mi configuraciond de nix

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,24 +1,10 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    -- En las versiones nuevas usamos 'opts' en lugar de llamar a un módulo que ya no existe
     opts = {
         highlight = { enable = true },
         indent = { enable = true },
         autotag = { enable = true },
-        ensure_installed = {
-            "lua",
-            "tsx",
-            "typescript",
-            "php",
-            "nix",
-            "vim",
-            "vimdoc",
-            "markdown",
-            "markdown_inline",
-            "regex",
-            "bash",
-        },
+        ensure_installed = {},
         auto_install = false,
     },
 }
