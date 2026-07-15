73ceac1a7da8610ee54ed36870f075bef199dd30
araooz <araozfali@gmail.com>
2026-07-15 13:53:42 -0500

    hypr: cambio a .lua aun no reviso el archivo de config solo lo pase a claude para q lo traduzca

--- nvim/lua/plugins/oneliners.lua
+++ nvim/lua/plugins/oneliners.lua
@@ -18,7 +18,7 @@ return {
         settings = {
             Lua = {
                 diagnostics = {
-                    globals = { "vim" } }
+                    globals = { "vim", "hl" } }
             }
         }
     })
