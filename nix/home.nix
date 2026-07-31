57901ec2745364c80e83eaeda0b555129845576b
araooz <araozfali@gmail.com>
2026-07-31 16:32:00 -0500

    nix: opacidad de kitty 0.6

--- nix/home.nix
+++ nix/home.nix
@@ -118,7 +118,7 @@
     enable = true;
     settings = {
       confirm_os_window_close = 0;
-      background_opacity = "0.4";
+      background_opacity = "0.6";
       shell = "zsh";
     };
   };
